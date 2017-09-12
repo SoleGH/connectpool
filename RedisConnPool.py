9833aa6ad6e7c8e2f8bf968b692c5d8bea7951f7
SoleGH <ybifei@163.com>
2017-09-12 10:19:38 +0800

    Create RedisConnPool.py

---
+++ RedisConnPool.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+import redis
+
+class Rcon(object):
+    """
+    管理connect 和 pool
+    """
+    pools = {}
+    rcons = {}
+    @staticmethod
+    def get_conn(db_num=0):
+        if db_num not in Rcon.rcons.keys():
+            if db_num not in Rcon.pools.keys():
+                Rcon.pools[db_num] = redis.ConnectionPool(host="10.9.40.114", port=6379, password="admin",
+                                                      db=db_num)
+                Rcon.rcons[db_num] = redis.Redis(connection_pool=Rcon.pools[db_num])
+                return Rcon.rcons[db_num]
+            else:
+                Rcon.rcons[db_num] = redis.Redis(connection_pool=Rcon.pools[db_num])
+                return Rcon.rcons[db_num]
+        else:
+            return Rcon.rcons[db_num]
